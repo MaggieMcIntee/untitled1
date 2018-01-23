68e785ac5f392ef7d00f3f4593db31e535c1623f
Maggie McIntee <Margaret.McIntee@yale.edu>
2018-01-23 18:31:05 -0500

    Initial commit

---
+++ Question1.py
@@ -0,0 +1,19 @@
+x = 17
+print('Setting x =', x)
+
+#interger
+y1=x
+print('y1 is a ' ,type(y1))
+
+#float
+y2 = float(x)
+print('y2 is a ' ,type(y2))
+
+#string
+y3=str(x)
+print('y3 is a' ,type(y3))
+
+#Boolean,'17>12'
+y4=bool(x>12)
+print('y4 is a ', type(y4))
+print(y4)
